41437d1941b3d17e603cc314862a5afc1427e631
Ashish Sheelavantar <ashish.sheelavantar@anz.com>
2021-05-03 19:04:14 +0530

    config change

--- config.py
+++ config.py
@@ -10,5 +10,7 @@ class Config(object):
     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "postgresql:///demo_user"
 
     # For postgressql created using user msashish and in CloudSQL
-    SQLALCHEMY_DATABASE_URI = "msashish:///sql-dev-cd1b2ba1"
+    #SQLALCHEMY_DATABASE_URI = "msashish:///sql-dev-cd1b2ba1"
+    SQLALCHEMY_DATABASE_URI = \
+        'postgresql+psycopg2://msashish:ShreeGanesh1@/postgres?host=/cloudsql/qwiklabs-gcp-00-aa77c6f5d862:us-central1:sql-dev-cd1b2ba1/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

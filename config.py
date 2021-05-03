1a4f9f77821027eddf55325a732db185d6ba7917
Ashish Sheelavantar <ashish.sheelavantar@anz.com>
2021-05-03 17:22:34 +0530

    added local and gcp DB uri

--- config.py
+++ config.py
@@ -5,5 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-never-know-anything'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "postgresql:///demo_user"
+
+    # For local test TODO: based on local or GCP we can configure either of them
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "postgresql:///demo_user"
+
+    # For postgressql created using user msashish and in CloudSQL
+    SQLALCHEMY_DATABASE_URI = "msashish:///sql-dev-917bf9b7"
     SQLALCHEMY_TRACK_MODIFICATIONS = False

8f32b1166a04723c45757921fa438375deb45007
Ashish Sheelavantar <ashish.sheelavantar@anz.com>
2021-05-09 19:31:09 +0530

    environment injection test

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ class Config(object):
     DB_USER = os.environ.get('DB_USER')
     DB_PASS = os.environ.get('DB_PASS')
 
+    print(DB_NAME, DB_USER, DB_PASS)
+
     # Simply connect to Cloud SQL Proxy sidecar @ 127.0.0.1:5432
     SQLALCHEMY_DATABASE_URI = \
         'postgresql+psycopg2://DB_USER:DB_PASS@127.0.0.1:5432/DB_NAME'
